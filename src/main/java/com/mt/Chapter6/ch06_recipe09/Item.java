b697de600760391899c3902645ad4df9ce575f3f
Tarasevich <m.tarasevich@global.local>
2024-10-02 17:21:32 +0300

    ch06_recipe07-09

---
+++ src/main/java/com/mt/Chapter6/ch06_recipe09/Item.java
@@ -0,0 +1,32 @@
+package com.mt.Chapter6.ch06_recipe09;
+
+public class Item {
+
+    private String title;
+    private String content;
+    /**
+     * @return the title
+     */
+    public String getTitle() {
+        return title;
+    }
+    /**
+     * @param title the title to set
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    /**
+     * @return the content
+     */
+    public String getContent() {
+        return content;
+    }
+    /**
+     * @param content the content to set
+     */
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+}
